1ca321fcc6c72cd58053400131ed4839c829ac6a
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-02-08 17:54:42 -0500

    manually merged Alex's additional flight endpoints
    (see https://github.com/brockuniera/microservice/commit/1eda009e26e6c40f9dccdcc5f037d6540f27cd8f
    )

--- src/main/java/com/ss/utopia/controller/FlightController.java
+++ src/main/java/com/ss/utopia/controller/FlightController.java
@@ -3,7 +3,6 @@
  */
 package com.ss.utopia.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,20 +27,35 @@ public class FlightController {
 	private FlightService flightService;
 	
 	@GetMapping(path = {"/", ""},produces= {"application/json", "application/xml"})
-	public ResponseEntity<List<Flights>> getAllAirports(){
+	public ResponseEntity<List<Flights>> getAllFlights(){
 		return ResponseEntity.ok(flightService.getAllFlights());
+	}
+	
+	@GetMapping(path = {"/number/{id}"},produces= {"application/json", "application/xml"})
+	public ResponseEntity<Flights> getSpecificFlight(@PathVariable("id") String id){
+		return flightService.getSpecificFlight(id)
+				.map(x -> ResponseEntity.ok(x))
+				.orElseGet(() -> ResponseEntity.notFound().build());
 	}
 	
 	@GetMapping(path = {"/from/{airportid}"},produces= {"application/json", "application/xml"})
 	public ResponseEntity<List<Flights>> getAllFlightsDepartingFromAirport(@PathVariable("airportid") String airportid){
 		List<Flights> tempList = flightService.getAllFlightsDepartingFromAirport(airportid);
 		return ResponseEntity.ok(tempList);
+		return ResponseEntity.ok(flightService.getAllFlightsDepartingFromAirport(airportid));
 	}
 	
-//	@GetMapping(path = {"/to/{airportid}"},produces= {"application/json", "application/xml"})
-//	public ResponseEntity<List<Flights>> getAllFlightsArrivingToAirport(@PathVariable("airportid") String airportid){
-//		List<Flights> tempList = flightService.getAllFlightsArrivingToAirport(airportid);
-//		return ResponseEntity.ok(tempList);
-//	}	
+	@GetMapping(path = {"/to/{airportid}"},produces= {"application/json", "application/xml"})
+	public ResponseEntity<List<Flights>> getAllFlightsArrivingToAirport(@PathVariable("airportid") String airportid){
+		return ResponseEntity.ok(flightService.getAllFlightsArrivingToAirport(airportid));
+	}
+
+	@GetMapping(path = {"/from/{airportid1}/to/{airportid2}"},produces= {"application/json", "application/xml"})
+	public ResponseEntity<List<Flights>> getAllFlightsBetweenAirports(
+			@PathVariable("airportid1") String airportid1,
+			@PathVariable("airportid2") String airportid2)
+	{
+		return ResponseEntity.ok(flightService.getAllFlightsBetweenAirports(airportid1, airportid2));
+	}
 
 }
--- src/main/java/com/ss/utopia/service/FlightService.java
+++ src/main/java/com/ss/utopia/service/FlightService.java
@@ -3,16 +3,12 @@
  */
 package com.ss.utopia.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
 import com.ss.utopia.dao.FlightDAO;
 import com.ss.utopia.model.Bookings;
 import com.ss.utopia.model.Flights;
@@ -26,14 +22,18 @@ import com.ss.utopia.model.Flights;
 public class FlightService {
 	
 	@Autowired
-	private FlightDAO flightDAO;
+	private FlightDAO dao;
 
 	public List<Flights> getAllFlights() {
-		return flightDAO.findAll();
+		return dao.findAll();
+	}
+	
+	public Optional<Flights> getSpecificFlight(String Number){
+		return Optional.ofNullable(dao.getFlightWithNumber(Number.toUpperCase())); // .toUpperCase() used to add case-insensitivity
 	}
 	
 	public Flights getFlightById(int flightId) {
-		Optional<Flights> found = flightDAO.findById(flightId);
+		Optional<Flights> found = dao.findById(flightId);
 		if (found.isPresent()) {
 			return found.get();
 		} else {
@@ -42,51 +42,17 @@ public class FlightService {
 	}
 	
 	public List<Flights> getAllFlightsDepartingFromAirport(String airportid){
-		return flightDAO.getAllFlightsDepartingFromAirport(airportid);
+		return dao.getAllFlightsDepartingFromAirport(airportid);
 	}
 	
 	public List<Flights> getAllFlightsArrivingToAirport(String airportid){
-		return flightDAO.getAllFlightsArrivingToAirport(airportid);
+		return dao.getAllFlightsArrivingToAirport(airportid);
 	}
 
-//	public List<Flights> getSpecificAirport(String airportIdentifier) {
-//		List<String> tempList = new ArrayList<String>();
-//		tempList.add(airportIdentifier);
-//		return flightDAO.findAllById(tempList);
-//	}
-
-	public String addAirport(Flights newAirport) {
-		try {
-			flightDAO.save(newAirport);
-			return "Success in adding" + newAirport.toString();
-		}
-		catch(Exception e){
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not add " + newAirport.toString());
-		}
+	public List<Flights> getAllFlightsBetweenAirports(String airportid1, String airportid2){
+		return dao.getAllFlightsBetweenAirports(airportid1.toUpperCase(), airportid2.toUpperCase());
 	}
 
-	public String updateAirport(Flights newAirport) {
-		try {
-			flightDAO.save(newAirport);
-			return "Updated " + newAirport.toString();
-		}
-		catch(Exception e){
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not update " + newAirport.toString());
-		}
-	}
-
-	public String deleteAirport(Flights newAirport) {
-		try {
-			flightDAO.delete(newAirport);
-			return "Deleted " + newAirport.toString();
-		}
-		catch(DataIntegrityViolationException e){
-			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Current airport is still in use. Please try again later " + newAirport.toString());
-		}
-		catch(Exception e) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not delete " + newAirport.toString());
-		}
-	}
 
 
 }
